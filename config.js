dadda81d5e04cb67af091803859a5b1bfa049153
Davit Barbakadze <jayarjo@gmail.com>
2013-05-09 22:12:56 +0400

    Drop TestSwarm and PageKite.

--- config.js
+++ config.js
@@ -6,25 +6,10 @@ module.exports = {
 		loglevel: 'silent'
 	},
 
-	testswarm: {
-		url: "SWARM_URL",
-		testsurl: 'BASE_TESTS_URL', // this will be automatically prepended to all relative test urls
-		username: 'USERNAME',
-		authToken: 'TOKEN',
-		pollInterval: 1000,
-		timeout: 1000 * 60 * 15 // 15 minutes
-	},
-
 	browserstack: {
 		username: "USERNAME",
 		password: "PASSWORD",
 		version: 2,
 		timeout: 480
-	},
-	
-	tunnel: {
-		url: "KITE_URL",
-		secret: "SECRET",
-		cmd: "pagekite.py --clean --defaults --backend=http/<port>:<url>:<localhost>:<localport>:<secret>"
 	}
 };
